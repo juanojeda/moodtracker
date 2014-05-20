8a74510c609f96794b617054cc3dd80826e640f8
juan-ojeda <juan.ojeda@kiandra.com.au>
2014-05-20 13:36:07 +1000

    hooked up firebase - now refactor to suit a db\!

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,81 +1,84 @@
+/*global _, Firebase*/
 'use strict';
 
-var _ = window._;
-
 angular.module('moodtrackerApp')
-  .controller('MainCtrl', function ($scope) {
-    
-    var moods = [
-        {
-            value: 0,
-            text: 'tell us how you feel',
-            moodClass: 'mood-none'
-        },
-        {
-            value: -3,
-            text: 'super unhappy',
-            moodClass: 'mood-super-unhappy'
-        },
-        {
-            value: -2,
-            text: 'very unhappy',
-            moodClass: 'mood-very-unhappy'
-        },
-        {
-            value: -1,
-            text: 'pretty unhappy',
-            moodClass: 'mood-pretty-unhappy'
-        },
-        {
-            value: 1,
-            text: 'pretty happy',
-            moodClass: 'mood-pretty-happy'
-        },
-        {
-            value: 2,
-            text: 'very happy',
-            moodClass: 'mood-very-happy'
-        },
-        {
-            value: 3,
-            text: 'super happy',
-            moodClass: 'mood-super-happy'
-        },
-    ];
+    .factory('moodService', ['$firebase', function($firebase){
+        var ref = new Firebase('https://kmoods.firebaseio.com/');
+        return $firebase(ref);
+    }])
+    .controller('MainCtrl', ['$scope', 'moodService', function ($scope, moodService) {
 
-    $scope.moodsArr = [];
+            $scope.moods = [
+                {
+                    value: 0,
+                    text: 'tell us how you feel',
+                    moodClass: 'mood-none'
+                },
+                {
+                    value: -3,
+                    text: 'super unhappy',
+                    moodClass: 'mood-super-unhappy'
+                },
+                {
+                    value: -2,
+                    text: 'very unhappy',
+                    moodClass: 'mood-very-unhappy'
+                },
+                {
+                    value: -1,
+                    text: 'pretty unhappy',
+                    moodClass: 'mood-pretty-unhappy'
+                },
+                {
+                    value: 1,
+                    text: 'pretty happy',
+                    moodClass: 'mood-pretty-happy'
+                },
+                {
+                    value: 2,
+                    text: 'very happy',
+                    moodClass: 'mood-very-happy'
+                },
+                {
+                    value: 3,
+                    text: 'super happy',
+                    moodClass: 'mood-super-happy'
+                },
+            ];
 
-    $scope.moods = moods;
-    //set to first item in moods
-    $scope.moodValue = 0;
-    // set a base mood level
-    $scope.averageMood = 0;
+            $scope.moodsArr = moodService;
 
-    function getCurrentMood(){
-        return $scope.moods[$scope.moodValue];
-    }
+            console.log(moodService);
+            //set to first item in moods
+            $scope.moodValue = 0;
+            // set a base mood level
+            $scope.averageMood = 0;
 
-    function setMood(moodValue){
+            function getCurrentMood(){
+                return $scope.moods[$scope.moodValue];
+            }
 
-        // can't set an unset mood
-        if (moodValue !== 0){
-            // take current mood, and push it to moodsArr
-            $scope.moodsArr.push(moodValue);
-        }
-    }
+            function setMood(moodValue){
 
-    function getAverageMood(moodsArr){
-        var moodsCount = moodsArr.length;
-        var calculateTotal = _.reduce(moodsArr, function(memo, val){ return memo + val;}, 0);
+                // can't set an unset mood
+                if (moodValue !== 0){
+                    // take current mood, and push it to moodsArr
+                    $scope.moodsArr.$add(moodValue);
+                }
+            }
 
-        var average = Math.round(calculateTotal / moodsCount * 100) / 100;
-        average = average.toString();
+            function getAverageMood(moodsArr){
+                var moodsCount = moodsArr.length;
+                var calculateTotal = _.reduce(moodsArr, function(memo, val){ return memo + val;}, 0);
 
-        return moodsArr.length ? average : null;
-    }
+                var average = Math.round(calculateTotal / moodsCount * 100) / 100;
+                average = average.toString();
 
-    $scope.currentMood = getCurrentMood;
-    $scope.setMood = setMood;
-    $scope.getAverageMood = getAverageMood;
+                return moodsArr.length ? average : null;
+            }
 
-  });
+            $scope.currentMood = getCurrentMood;
+            $scope.setMood = setMood;
+            $scope.getAverageMood = getAverageMood;
+
+        }]);
