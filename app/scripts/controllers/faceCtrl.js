1dacfa25b497976aa7debd8483dd2a6f1dc7fa96
Juan Ojeda <juan.ojeda@kiandra.com.au>
2015-03-07 21:24:29 +1100

    fixed face flippy issue

--- app/scripts/controllers/faceCtrl.js
+++ app/scripts/controllers/faceCtrl.js
@@ -9,13 +9,13 @@ angular.module('moodtrackerApp')
         var face = new Snap('#face');
 
         $scope.mouths = {
-            happy3: 'M10 10 C 5 70, 95 70, 90 10',
-            happy2: 'M10 20 C 25 60, 75 60, 90 20',
-            happy1: 'M10 30 C 30 50, 70 50, 90 30',
-            neutral: 'M10 40 C 50 40, 50 40, 90 40',
-            upset1: 'M 90 50 C 70 30, 30 30, 10 50',
-            upset2: 'M90 60 C 75 20, 25 20, 10 60',
-            upset3: 'M90 70 C 95 10, 5 10, 10 70'
+            happy3:  'M10 10 C05 70, 95 70, 90 10',
+            happy2:  'M10 20 C25 60, 75 60, 90 20',
+            happy1:  'M10 30 C30 50, 70 50, 90 30',
+            neutral: 'M10 40 C50 40, 50 40, 90 40',
+            upset1:  'M10 50 C30 30, 70 30, 90 50',
+            upset2:  'M10 60 C25 20, 75 20, 90 60',
+            upset3:  'M10 70 C05 10, 95 10, 90 70'
         };
 
         var mouth = face.el('path', {
@@ -27,7 +27,6 @@ angular.module('moodtrackerApp')
         function setMouthShape(moods) {
             var oldMouth = $scope.mouths[moods[1]];
             var newMouth = $scope.mouths[moods[0]];
-            console.log(oldMouth + '\n' + newMouth);
             if (oldMouth !== newMouth){
                 mouth.animate({d: newMouth}, 300);
             }
